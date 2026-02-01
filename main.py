93a64cf868ddb3ee0a0c479a97f446b79924622c
Abdul Rehman Butt <arbutt4443@gmail.com>
2026-02-01 11:43:10 +0500

    Created new file to perform filtering tasks

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import fileCleaning
 import fileReading
+import filters
 
 filename = "gdp_with_continent_filled.csv"
-
-
 df = fileReading.readCSVfile(filename)
+
+
 if df is not None:
     df = fileCleaning.cleanFile(df)
-    print(df.head(15))
+    #print(df.head())
+
+df_filtered = filters.filterYearRange(df, 1997, 2000)
+print(df_filtered)
